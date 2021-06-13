890dbd56e22709e7a84f22a7bc3dc1b999e890ed
Leonardo Souza <leogsouza@gmail.com>
2021-06-13 17:14:01 -0300

    connecting to mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import 'express-async-errors';
+import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -23,7 +24,24 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler)
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-svc:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to mongodb')
+    } catch(err) {
+        console.error(err);
+    }
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!!!')
-})
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!')
+    })
+    
+}
+
+start()
+
+
