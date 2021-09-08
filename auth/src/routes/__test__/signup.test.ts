4c3d5bb3cdcd4d94853358028b8ea5a62980e4ad
Leonardo Souza <leogsouza@gmail.com>
2021-09-07 23:09:30 -0300

    Change node env during tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -20,3 +20,33 @@ it('returns a 400 with an invalid email', async () => {
         })
         .expect(400)
 })
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+    
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+})
+
+it('sets a cookie after successful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'abc12345'
+        })
+        .expect(201)
+
+    expect(response.get('Set-Cookie')).toBeDefined()
+})
